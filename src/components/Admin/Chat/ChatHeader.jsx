89c7cd193df95b2ac390cfca17367f1a7ac6feb6
Rupam Kairi <33358148+rupamkairi@users.noreply.github.com>
2021-01-12 20:05:16 +0530

    chat bubble rounded

--- src/components/Admin/Chat/ChatHeader.jsx
+++ src/components/Admin/Chat/ChatHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ChatHeader(props) {
   return (
     <div>
-      <span className="fs-6 fw-bold">{props.fullname}</span>
+      <span className="fs-5 fw-bold text-truncate">{props.fullname}</span>
     </div>
   );
 }
--- src/components/Admin/Chat/Thread.jsx
+++ src/components/Admin/Chat/Thread.jsx
@@ -5,8 +5,8 @@ export default function Thread(props) {
     <div
       className={
         (props.position === "right"
-          ? "align-self-end ms-5 rounded-pill border bg-primary bg-gradient text-white"
-          : "align-self-start me-5 rounded-pill border bg-secondary bg-gradient text-white") +
+          ? "align-self-end ms-5 bubble-sender bg-primary bg-gradient text-white"
+          : "align-self-start me-5 bubble-sendee bg-secondary bg-gradient text-white") +
         " px-4 py-2 my-2 "
       }
     >
